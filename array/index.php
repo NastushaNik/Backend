03004f344f5de41cdd843bbebe35eb9e85e3b3d1
NastushaNik <nastushanik@gmail.com>
2017-09-29 20:52:19 +0300

    First commit

--- array/index.php
+++ array/index.php
@@ -9,12 +9,5 @@ $complex_arr2 = array();
 $complex_arr2[2] = array('c'=>'ivan');
 $complex_arr2[3] = array('a'=>'igor');
 $complex_arr2[4] = array('b'=>'vasya');
-
-function myCamp2($a, $b){
-    if ($a == $b){
-        return 0;
-    }
-    return $a > $b ? 1 : -1;
-}
-uksort($complex_arr2, myCamp2);
-print_r($complex_arr2);
+echo "Hello World";
+$a = 0;
