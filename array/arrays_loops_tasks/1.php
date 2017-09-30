c8a738de5e39bb4f26f227053429205622d70205
NastushaNik <nastushanik@gmail.com>
2017-09-30 09:36:41 +0300

    1.php

---
+++ array/arrays_loops_tasks/1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yoga
+ * Date: 30.09.2017
+ * Time: 9:27
+ */
+$arr1 = array('html','css','php','js','jq');
+foreach ($arr1 as $value){
+print_r($value.'<br>');
+}
